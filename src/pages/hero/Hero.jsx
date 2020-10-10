83d8d94aa03a3f7fdd340d8faffd0a5040f95734
eastflag <eastflag@gmail.com>
2020-10-11 01:40:57 +0900

    heroes 상세보기 - 화면 UI 바인딩

--- src/pages/hero/Hero.jsx
+++ src/pages/hero/Hero.jsx
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import api from "../../utils/api";
 
 export const Hero = (props) => {
+  const [hero, setHero] = useState(null);
+
+  useEffect(() => {
+    getHero(props.match.params.id);
+  }, []);
+
+  const getHero = async (id) => {
+    let response = await api.get(`/api/user/hero/${id}`);
+    console.log(response);
+
+    if (response.data) {
+      setHero(response.data);
+    }
+  }
+
   return (
-    <div>
-      Hero works!!
-    </div>
+    hero ?
+      <div>
+        <table className="table">
+          <tbody>
+          <tr>
+            <th scope="row">Name</th>
+            <td>{hero.name}</td>
+          </tr>
+          <tr>
+            <th scope="row">Email</th>
+            <td>{hero.email}</td>
+          </tr>
+          <tr>
+            <th scope="row">Sex</th>
+            <td>{hero.sex}</td>
+          </tr>
+          <tr>
+            <th scope="row">Country</th>
+            <td>{hero.country}</td>
+          </tr>
+          <tr>
+            <th scope="row">Power</th>
+            <td>{hero.powers.toString()}</td>
+          </tr>
+          <tr>
+            <th scope="row">Photo</th>
+            <td>
+              {
+                hero.photo ? <img src={hero.photo} alt={hero.name} style={{maxWidth: '100%'}}></img> : ''
+              }
+            </td>
+          </tr>
+          </tbody>
+        </table>
+        <hr className="my-5" />
+      </div>
+      : ''
   )
 }
