b82785fef4f35147d9e924f78e2d74e75d9a0008
eastflag <eastflag@gmail.com>
2020-10-11 04:24:22 +0900

    heroes 삭제하기

--- src/pages/hero/Hero.jsx
+++ src/pages/hero/Hero.jsx
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 import {View} from "./View";
 import {Edit} from "./Edit";
+import {Modal, ModalHeader, ModalBody, ModalFooter, Button} from "reactstrap";
+import api from "../../utils/api";
 
 export const Hero = (props) => {
   console.log('View: ', props);
@@ -8,6 +10,18 @@ export const Hero = (props) => {
 
   const handleEditMode = (e) => {
     setIs_edit(!is_edit);
+  }
+
+  const [modal, setModal] = useState(false);
+  const toggle = () => setModal(!modal);
+
+  const handleDelete = () => {
+    toggle();
+    api.delete(`/api/admin/hero?id=${props.match.params.id}`)
+      .then(response => {
+        console.log(response.data);
+        props.history.push('/heroes/hero'); // this.props.router.push('/heroes/hero'); 3.0.0+
+      });
   }
 
   return (
@@ -18,12 +32,22 @@ export const Hero = (props) => {
           { is_edit ? <button className="btn btn-info" onClick={handleEditMode}>취소</button> :
             <button className="btn btn-success" onClick={handleEditMode}>수정</button>
           }
-          <button className="btn btn-danger ml-3">삭제</button>
+          <button className="btn btn-danger ml-3" onClick={toggle}>삭제</button>
         </div>
       </div>
       {
         is_edit ? <Edit id={props.match.params['id']}/> : <View id={props.match.params['id']} />
       }
+      <Modal isOpen={modal} toggle={toggle}>
+        <ModalHeader toggle={toggle}>삭제</ModalHeader>
+        <ModalBody>
+          {} 삭제하시겠습니까?
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={handleDelete}>OK</Button>{' '}
+          <Button color="secondary" onClick={toggle}>Cancel</Button>
+        </ModalFooter>
+      </Modal>
     </>
   );
 }
