d9c9004c44e4f26b1161e33d77a6532f8aecdba0
eastflag <eastflag@gmail.com>
2020-10-10 21:10:34 +0900

    heroes list - public asset 적용하기

--- src/pages/heroes/Heroes.jsx
+++ src/pages/heroes/Heroes.jsx
@@ -19,7 +19,8 @@ export const Heroes = (props) => {
       {heroes.map(hero => (
         <li key={hero.id} className="row align-items-center m-0">
           <div className="col-1 py-2">
-            <img src={hero.photo} alt={hero.name} className="img-fluid rounded-circle"></img>
+            <img src={hero.photo ? hero.photo : process.env.PUBLIC_URL + '/images/face-black-18dp.svg'}
+                 alt={hero.name} style={{width: '100%'}}></img>
           </div>
           <span className="col">{hero.name}</span>
         </li>
