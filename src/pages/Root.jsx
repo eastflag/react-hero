81c8226e0ccd9b1c655626d1cb6b365eca71f1e2
eastflag <eastflag@gmail.com>
2020-10-10 14:04:19 +0900

    라우팅  2 - switch 추가

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import {Heroes} from "./Heroes";
 import {Home} from "./Home";
 import {Scoreboard} from "./Scoreboard";
@@ -9,10 +9,12 @@ export const Root = (props) => {
   return (
     <BrowserRouter>
       <p>공통메뉴 영역</p>
-      <Route path="/" component={Home}></Route>
-      <Route path="/heroes" component={Heroes}></Route>
-      <Route path="/scoreboard" component={Scoreboard}></Route>
-      <Route path="/product" component={Product}></Route>
+      <Switch>
+        <Route path="/" component={Home}></Route>
+        <Route path="/heroes" component={Heroes}></Route>
+        <Route path="/scoreboard" component={Scoreboard}></Route>
+        <Route path="/product" component={Product}></Route>
+      </Switch>
     </BrowserRouter>
   )
 }
